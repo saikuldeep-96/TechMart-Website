ee71ec22253d8eb1e655ebcc1efd6af44128247c
saikuldeep-96 <saikuldeep996@gmail.com>
2025-01-29 20:45:11 -0500

    third

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./home";
 import LoginSignup from "./loginSignup";
+import Products from "./products";
+import Laptops from "./laptops";
+import Mobiles from "./mobiles";
+// import ProductDetails from "./productDetails"; 
+// import Cart from "./cart";
+// import Checkout from "./checkout"; 
+// import Payment from "./payment"; 
+// import Orders from "./orders"; 
+// import AdminDashboard from "./admin";                /updated routes and added laptops and products pages/
+//
+
 
 function App() {     {/* Routes updated */}
   return (
     <Router>                   
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
 
-          <Route path="/login" element={<LoginSignup />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login-signup" element={<LoginSignup />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/laptops" element={<Laptops />} />
+          <Route path="/mobiles" element={<Mobiles />} />
+          
+
         </Routes>
       </div>
     </Router>
